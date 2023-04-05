79adea746f30f01458e8959126f556437ce415da
Trufanov Nikita <trufanovwork@gmail.com>
2023-04-05 14:58:26 +0300

    add sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
+
 end

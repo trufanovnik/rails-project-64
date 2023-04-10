76380f023c31bbf070f2d82a6d54c29b1c93b5e4
Trufanov Nikita <trufanovwork@gmail.com>
2023-04-10 20:07:09 +0300

    add some relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :post_comments, dependent: :destroy
+  has_many :post_comments, class_name: 'PostComment', dependent: :destroy
 end
